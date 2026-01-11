f5307cd7679659e3be324ad9adc7368953815cc8
gabriela <gabrielapoves01@gmail.com>
2026-01-11 19:05:03 +0100

    improve logic edit

--- todo-app/src/components/TodoList/TodoItem/index.jsx
+++ todo-app/src/components/TodoList/TodoItem/index.jsx
@@ -4,24 +4,28 @@ export const TodoItem = ({ todo, onHandleDeleteTodo, onHandleEditTodo }) => {
   const [showEditInput, setShowEditInput] = useState(false);
   const [inputEditValue, setInputEditValue] = useState(todo.text);
 
-  const handleEdit = (id, value, e) => {
+  const handleEdit = ({ todo, e }) => {
     e.preventDefault();
-    onHandleEditTodo(id, value);
+    onHandleEditTodo(todo.id, inputEditValue);
     setShowEditInput(false);
   };
 
-  return showEditInput ? (
-    <form onSubmit={(e) => handleEdit(todo.id, inputEditValue, e)}>
+  if (!showEditInput)
+    return (
+      <li className="todo-list__item">
+        {todo.text}{" "}
+        <button onClick={() => onHandleDeleteTodo(todo.id)}>X</button>
+        <button onClick={() => setShowEditInput(true)}>Edit</button>
+      </li>
+    );
+
+  return (
+    <form onSubmit={(e) => handleEdit({ todo, e })}>
       <input
         value={inputEditValue}
         onChange={(e) => setInputEditValue(e.target.value)}
       />
       <button>Guardar</button>
     </form>
-  ) : (
-    <li className="todo-list__item">
-      {todo.text} <button onClick={() => onHandleDeleteTodo(todo.id)}>X</button>
-      <button onClick={() => setShowEditInput(true)}>Edit</button>
-    </li>
   );
 };
